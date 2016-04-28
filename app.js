3a440277a57f48dc902a9fe9b34a3d8277146758
Samuel Stone <sjstone1987@gmail.com>
2016-04-27 20:46:03 -0400

    added custom directive ideogram

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 'use strict';
 
-var ideogram = angular.module('ideogram',[]);
+var height = 30;
+var fullWidth = 800;
+var widthOffset = 125;
+var width = fullWidth - widthOffset;
+var radius = height / 2;
+var dur = 1500;
+
+var app = angular.module('app',[]);
 
 function MainCtrl ($scope,$http) {
   var url = 'https://api.solvebio.com/v1/datasets/ISCN/1.1.0-2015-01-05/Ideograms/data?access_token=98e8f6ba570311e4bab59f6dc3060e21&limit=10000';
@@ -15,17 +22,13 @@ function MainCtrl ($scope,$http) {
    ],
    'limit': "10000",
    'filters': [{and: [
-    // ["genomic_coordinates.chromosome", "7"],
     ["band_level", "550"]
    ]}]
   };
-
-  $scope.height = 20;
-  $scope.width = 500;
-  var widthOffset = 75;
-  $scope.radius = $scope.height / 2;
-  $scope.viewbox = "0 0 "+$scope.width+" "+$scope.height;
+  
+  $scope.viewbox = "0 0 "+fullWidth+" "+height;
   $scope.arms = [];
+  $scope.bands = [];
 
   $http.post(url,data).
     then(function(response) {
@@ -56,9 +59,8 @@ function MainCtrl ($scope,$http) {
       });
       $scope.chromosomes = _.range(1,23);
       $scope.chromosomes.push('X','Y');
-
   })
-  
+
   $scope.updateChromosome = function (selectedChromosome){
     var p_arm = $scope.arms.filter(function(arm){
       return arm.chromosome == selectedChromosome && arm.arm == 'p';
@@ -66,52 +68,37 @@ function MainCtrl ($scope,$http) {
     var q_arm = $scope.arms.filter(function(arm){
       return arm.chromosome == selectedChromosome && arm.arm == 'q';
     })[0];
-    var bands = $scope.bands.filter(function(band){
+     $scope.selectedBands = $scope.bands.filter(function(band){
       return band.genomic_coordinates.chromosome == selectedChromosome;
     });
 
-    d3.selectAll('rect').remove();
     d3.selectAll('g').remove();
 
     var xScale = d3.scale.linear()
       .domain([0,q_arm.stop])
-      .range([0,$scope.width - widthOffset]);
+      .range([0,fullWidth - widthOffset]);
 
     var densityScale = d3.scale.linear()
       .domain([0,100])
       .range([0.0,1.0]);
     
-    // // p arm
-    // var p_arm_svg = d3.select("#ideogram")
-    //   .selectAll('rect')
-    //   .data('p_arm');
-
-    // //p_arm_svg.exit().remove();
-
-    // p_arm_svg.enter()
-    //   .append('rect')
-    //   .attr('x',0)
-    //   .attr('width',xScale(p_arm.stop))
-    //   .attr('class','arm');
-
      // p arm
-    d3.select("#ideogram")
-      .append('rect')
-      .attr('x', 0)
+    d3.selectAll(".p_arm")
+      .transition()
+      .duration(dur)
       .attr('width',xScale(p_arm.stop))
-      .attr('class','arm');
 
     // q arm
-    d3.select("#ideogram")
-      .append('rect')
+    d3.selectAll(".q_arm")
+      .transition()
+      .duration(dur)
       .attr('x', xScale(q_arm.start))
       .attr('width',xScale(q_arm.stop - q_arm.start))
-      .attr('class','arm');
 
     // band container
-    d3.select('#ideogram')
+    d3.selectAll('svg')
       .selectAll('g')
-      .data(bands)
+      .data($scope.selectedBands)
       .enter()
       .append('g')
       .attr("transform", function (band){ 
@@ -120,14 +107,10 @@ function MainCtrl ($scope,$http) {
     // band
     d3.selectAll('g')
       .append('rect')
-      .attr('width',0)
+      .attr('fill-opacity',0)
       .attr('width', function(band){
         return xScale(band.genomic_coordinates.stop - band.genomic_coordinates.start);
       })
-      .attr('fill-opacity',function(band){
-        return densityScale(band.density);
-      })
-      .attr('class','band')
       .on("mouseover", function() { 
         d3.select(this).classed('active', true);
         d3.select(this.parentNode).selectAll('text').classed('active', true);
@@ -135,45 +118,58 @@ function MainCtrl ($scope,$http) {
       .on("mouseout", function() { 
         d3.select(this).classed('active', false);
         d3.select(this.parentNode).selectAll('text').classed('active', false);
-      });
+      })
+      .transition()
+      .duration(dur)
+      .attr('fill-opacity',function(band){
+        return densityScale(band.density);
+      })
+      .attr('class','band')
 
     // band label
     d3.selectAll('g').append('text').attr('y',"4em").text(function(band){ return "Band: "+ band.band_label });
-    d3.selectAll('g').append('text').attr('y',"5em").text(function(band){ return "Start: "+ band.genomic_coordinates.start });
-    d3.selectAll('g').append('text').attr('y',"6em").text(function(band){ return "Start: "+ band.genomic_coordinates.stop });
-    d3.selectAll('g').append('text').attr('y',"7em").text(function(band){ return "Density: "+ band.density });
+    d3.selectAll('g').append('text').attr('y',"5em").text(function(band){ return "Start Coordinate: "+ band.genomic_coordinates.start });
+    d3.selectAll('g').append('text').attr('y',"6em").text(function(band){ return "Stop Coordinate: "+ band.genomic_coordinates.stop });
+    d3.selectAll('g').append('text').attr('y',"7em").text(function(band){ return "Density (BPHS): "+ band.density });
 
-    // format all arms and bands
+    // format arms and bands
     d3.selectAll('rect')
       .attr('y',0)
-      .attr('height',$scope.height)
+      .attr('height',height)
     d3.selectAll(".arm")
-      .attr('rx',$scope.height / 2);
+      .attr('rx',radius);
 
-    // telomere and centromere cover stubs
-    var radius = $scope.radius;
-    var height = $scope.height;
-    var width = $scope.width-widthOffset;
-    
-    d3.select('#ideogram').append('path')
+    // telomere and centromere "caps"
+    d3.selectAll('svg').append('path')
      .attr('d',"M "+radius+",0 A"+radius+","+radius+" 0 0,0 "+radius+","+height+" L0 "+height+" L0 0 Z");
 
-   d3.select('#ideogram').append('path')
+   d3.selectAll('svg').append('path')
     .attr('d', "M"+(xScale(p_arm.stop)-radius)+",0 A"+radius+","+radius+" 0 0,1 "+(xScale(p_arm.stop)-radius)+","+height+" L"+(xScale(p_arm.stop)+radius)+","+height+" A"+radius+","+radius+" 0 0,1 "+(xScale(p_arm.stop)+radius)+",0 L"+(xScale(p_arm.stop)-radius)+" 0 Z");
 
-   d3.select('#ideogram').append('path')
+   d3.selectAll('svg').append('path')
     .attr('d', "M"+(width-radius)+",0 A"+radius+","+radius+" 0 0,1 "+(width-radius)+","+height+" L"+width+" "+height+" L"+width+" 0 Z");
   }
-
 }
 
 angular
-  .module('ideogram')
+  .module('app')
   .controller('MainCtrl', MainCtrl)
-  // .directive('ideogram', function(){
-  //   return {
-  //     templateUrl: 'templates/ideogram.html'
-  //   };
-  // })
-;
+  .directive('ideogram', function($window,$timeout){
+    return {
+      restrict:'E',
+      template:"<svg viewbox='0 0 "+fullWidth+" "+(height * 3)+"'></svg>",
+      link: function(scope, elem, attrs){
 
+        var d3 = $window.d3;
+        var rawSvg = elem.find("svg")[0];
+        var svg = d3.select(rawSvg);
+        
+        svg.append('rect')
+          .attr('x',0)
+          .classed('p_arm arm', true)
+
+        svg.append('rect')
+          .classed('q_arm arm', true)
+      }
+    };
+  });
